43280e6775cdf0394e2cb626362d7509a333cd83
Randy Wind <wind.randy@gmail.com>
2026-09-22 14:54:41 +0200

    parkiet support for json input as file list

--- custom/pipeline-parkiet.py
+++ custom/pipeline-parkiet.py
@@ -1,4 +1,4 @@
-import torch, sys, json, os, soundfile, re
+import torch, sys, json, os, soundfile, re, time
 from pathlib import Path
 from transformers import AutoProcessor, DiaForConditionalGeneration
 import numpy as np
@@ -22,14 +22,18 @@ class Settings(VActSettingsBase):
     def __init__(self):
         super().__init__()
         self.guidance = 3.0
-        self.output = "../_out/audio/{name}/{name}_{stem}{data}{variant}.mp3"
+        self.output = "../_out/audio/{stem}/{id}{data}{variant}.mp3"
         self.variant = "_saskia"
         self.prompt = "[S2]"
         self.ref_audio = ""
         self.ref_text = ""
+        self.skip = -1
+        self.untill = 10
+        self.merge_attr = 'id'
+        self.text_attr = 'text'
         #self.split = '.?!\n\r'
         #self.chunks = True
-        #self.pause = 0.08
+        self.pause = 0.08
 
 class VActParkietPipeline(VActPipelineBase):
     def __init__(self):
@@ -71,15 +75,16 @@ class VActParkietPipeline(VActPipelineBase):
                 processor = AutoProcessor.from_pretrained(str(model_config["repo_id"]), trust_remote_code=True, token=api_key)
                 predictor  = DiaForConditionalGeneration.from_pretrained(str(model_config["repo_id"]), trust_remote_code=True, torch_dtype=torch_dtype, token=api_key).to(device=device, dtype=torch_dtype)
 
-                # sample_rate = processor.feature_extractor.sampling_rate
-                # silence = torch.zeros(
-                #     int(sample_rate * settings.pause),
-                #     dtype=torch.float32,
-                #     device=device
-                # )
-                # regex = re.compile(f'("[^"]*")|[{re.escape(settings.split)}]') if settings.split else r'("[^"]*")'
+                sample_rate = processor.feature_extractor.sampling_rate
+                silence = torch.zeros(
+                    int(sample_rate * settings.pause),
+                    dtype=torch.float32,
+                    device=device
+                )
+                
                 ref_audio = None
                 ref_text = ""
+                _t0 = time.perf_counter()
                 if settings.ref_audio:
                     ref_audio, ref_sr = soundfile.read(settings.ref_audio.replace("{variant}",settings.variant), dtype="float32")
                     ref_text = Path(settings.ref_text).read_text(encoding="utf-8").strip() + " "
@@ -87,49 +92,50 @@ class VActParkietPipeline(VActPipelineBase):
                 output = self.format_output(settings.output, settings.name, settings.variant, pipe_index)
                 for index, text_file in enumerate(text_files):
                     file_path = Path(text_file)
-                    text = file_path.read_text(encoding="utf-8").strip()
+                    b_json = file_path.suffix.lower() == ".json"
+                    _raw = file_path.read_text(encoding="utf-8").strip()
+                    data = json.loads(_raw) if b_json else [_raw]
+                    _id = data[0].get(settings.merge_attr) if data and isinstance(data[0], dict) else None
+                    _merge = []
+                    for _index, entry in enumerate(data):
+                        b_skip = (settings.untill > 0 and _index > settings.untill) or settings.skip > _index
+                        if b_skip: continue;
 
-                    # chunks = [
-                    #     # (chunk.strip(), chunk[0] == '"')
-                    #     chunk.strip()
-                    #     for chunk in regex.split(text)
-                    #     if chunk and chunk.strip()
-                    # ] if settings.chunks else [text] #[(text, text[0] == '"')]
-                    # parts = []
-                    
-                    # for chunk in chunks:
-                    inputs = processor(
-                        text=(settings.prompt + " " if settings.prompt else "") + ref_text + text,# + chunk,
-                        audio=ref_audio,
-                        padding='max_length',
-                        return_tensors="pt"
-                    ).to(device)
+                        text = entry[settings.text_attr] if b_json else entry
+                        id = entry[settings.merge_attr] if b_json and settings.merge_attr else None
 
-                    prompt_len = processor.get_audio_prompt_len(
-                        inputs["decoder_attention_mask"]
-                    )
+                        inputs = processor(
+                            text=(settings.prompt + " " if settings.prompt else "") + ref_text + text,# + chunk,
+                            audio=ref_audio,
+                            padding='max_length',
+                            return_tensors="pt"
+                        ).to(device)
 
-                    outputs = predictor.generate(
-                        **inputs,
-                        max_new_tokens=3072,
-                        guidance_scale=settings.guidance,
-                        temperature=1.4,
-                        top_p=0.90,
-                        top_k=50,
-                    )
+                        prompt_len = processor.get_audio_prompt_len(
+                            inputs["decoder_attention_mask"]
+                        )
+                        # TODO maybe sliding frame pick a part of previous entry ending
+                        outputs = predictor.generate(
+                            **inputs,
+                            max_new_tokens=3072,
+                            guidance_scale=settings.guidance,
+                            temperature=1.4,
+                            top_p=0.90,
+                            top_k=50,
+                        )
+                        audio = processor.batch_decode(outputs, audio_prompt_len=prompt_len)
 
-                    audio = processor.batch_decode(outputs, audio_prompt_len=prompt_len)
-                        #parts.append(_audio[0])
+                        # TODO add while to merge while group attr matches finally merge and write
 
-                    #audio = torch.cat(parts, dim=0)
-                    _output = output.replace("{stem}", file_path.stem)
-
-                    if audio is not None:
-                        _output_vo = Path(_output.replace("{data}", "_vo"))
-                        _audio_path = settings.base_path / _output_vo
-                        _audio_path.parent.mkdir(parents=True, exist_ok=True)
-                        processor.save_audio(audio, _audio_path)
-                        print(True, len(audio), _audio_path.resolve())
+                        _output = output.replace("{stem}", file_path.stem).replace("{id}", f"{_index}_{id}")#.replace("{id}", id if id else settings.name)
+                        if audio is not None:
+                            _output_vo = Path(_output.replace("{data}", "_vo"))
+                            _audio_path = settings.base_path / _output_vo
+                            _audio_path.parent.mkdir(parents=True, exist_ok=True)
+                            processor.save_audio(audio, _audio_path)
+                            _tn = time.perf_counter()
+                            print(True, len(audio), len(text), _tn - _t0, _audio_path.resolve())
+                            _t0 = _tn
                            
         return True
 
